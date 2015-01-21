afc5819c9dbb4618dd057ca8de0cd2e49bcfe09a
Nathan Ross Powell <nathanrosspowell@gmail.com>
2015-01-20 23:33:35 -0500

    Basic insta jump

--- src/logic.ino
+++ src/logic.ino
@@ -1,9 +1,6 @@
 void move( Player& p1 )
 {
-    if (g_gb.buttons.repeat(BTN_UP, 1))
-    {
-        p1.pos.y -= 1;
-    }
+    /*
     else if (g_gb.buttons.repeat(BTN_RIGHT, 1))
     {
         p1.pos.x += 1;
@@ -15,5 +12,24 @@ void move( Player& p1 )
     else if (g_gb.buttons.repeat(BTN_LEFT, 1))
     {
         p1.pos.x -= 1;
+    }
+    */
+    const short maxHeight = 45 - p1.bounds.size.y;
+    // Clamp
+    if ( p1.pos.y > maxHeight )
+    {
+        p1.pos.y = maxHeight;
+    }
+
+    if ( p1.pos.y == maxHeight )
+    {
+        if (g_gb.buttons.repeat(BTN_UP, 1))
+        {
+            p1.pos.y -= 10;
+        }
+    }
+    else
+    {
+        p1.pos.y += 1;
     }
 }
--- src/player.h
+++ src/player.h
@@ -10,12 +10,14 @@ public:
     Rect head;
     Rect body;
     Rect legs;
+    Rect bounds;
 
     Player() 
         : pos ( 10, 10 )
         , head( 2, 0, 2, 2 )
         , body(0, 2, 6, 8 )
         , legs( 2, 10, 2, 8 )
+        , bounds( 0, 0, 6, 18 )
     {
     }
 };
